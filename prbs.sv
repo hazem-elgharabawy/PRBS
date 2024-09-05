44c718e13c92af89b3e9629e9e6ee719d27a4919
HazemElgharabawy <hazem.gharabawy6@gmail.com>
2024-09-05 16:15:03 +0300

    lsfr

---
+++ prbs.sv
@@ -0,0 +1,29 @@
+module PRBS (
+    input bit CLK,
+    input logic RSTn,
+    input logic [7:0] in,
+    input logic [7:0] n,
+    output logic [7:0] ouet
+);
+    reg [15:0] LSFR;
+    reg start;
+    reg [7:0] inner_counter;
+    reg [7:0] outer_counter;
+    always @(posedge CLK or negedge RSTn ) begin
+        if (!RSTn) begin
+            LSFR <= 8'h11;
+            start <= 1'b0;
+            inner_counter <= 0;
+            outer_counter <= 0;
+        end
+        else if (!start) begin
+
+        end
+        else begin
+            LSFR <= {LSFR,(LSFR[14]^LSFR[15])};
+        end
+    end
+
+    
+
+endmodule
